ae8f22326c56c1552b2a34ff73f503bff1907736
Matthew Moore <matt@thrivesmart.com>
2014-09-24 17:37:57 -0700

    make production server static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Lobsters::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  # Make assets work on heroku
+  config.serve_static_assets = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
